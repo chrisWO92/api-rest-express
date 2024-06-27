f363422ecd0ef5b7cc96e5f8c65676c76ec3d3bd
chriswo92 <chrisworkingonline@gmail.com>
2024-06-26 23:07:58 -0300

    adding myswl

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -5,14 +5,14 @@ const setupModels = require('../db/models')
 
 const USER = encodeURIComponent(config.dbUser)
 const PASSWORD = encodeURIComponent(config.dbPassword)
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
 // Se crea una instancia de Sequilize y se le pasa la URI de esta manera.
 // Esta librería va a gestionar la conexión por nosotros sólo pasansole este parámetro,
 // A diferencia con el pool que había que indicarle qué tipo de parámetro era
 // como un par llave-valor
 const sequelize = new Sequelize(URI, {
-    dialect: 'postgres',
+    dialect: 'mysql',
     logging: true,
 })
 
